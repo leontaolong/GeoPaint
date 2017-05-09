d5c322705a2e0371d549ca4020b5b0d1fd3b4619
leontaolong <longt8@uw.edu>
2017-05-08 18:28:02 -0700

    Add menu options in MapActivity

--- Geopaint/app/src/main/java/edu/uw/longt8/geopaint/MapsActivity.java
+++ Geopaint/app/src/main/java/edu/uw/longt8/geopaint/MapsActivity.java
@@ -1,7 +1,13 @@
 package edu.uw.longt8.geopaint;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +16,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "Map Activity";
     private GoogleMap mMap;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mapFragment.setRetainInstance(true); //retain data even when rotate the screen
+        mapFragment.setHasOptionsMenu(true); //show option menu at the action bar
     }
 
 
@@ -42,5 +52,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.pen:
+                Log.v(TAG, "Select Pen");
+//                changePenStatus(item);
+                return true;
+            case R.id.picker:
+                Log.v(TAG, "Select Picker");
+//                showColorPicker();
+                return true;
+            case R.id.save:
+                Log.v(TAG, "Select Save");
+//                saveDrawing();
+                return true;
+            case R.id.share:
+                Log.v(TAG, "Select Share");
+                //instantiate the ShareActionProvider for share
+                mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+//                handleShareFile(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
